0fad7f6eca5ebfbbcf3d7f98aa4d7447770c82ff
seungkyu.han <seungkyu.han@emotion.co.kr>
2023-09-21 15:22:24 +0900

    directory add, 추후 .git keep 제거

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [testMessage, setTestMessage] = useState([]);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setTestMessage(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,9 +28,12 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
+        <ul>
+          {testMessage.map((val, idx) => (
+            <li key={`${idx}-${val}`}>{val}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
